5d4aab000e8ea680e0a5ed2ad0af7086d21bd801
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-12-30 23:06:44 +0300

    feat: setup running server

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import logger from "morgan";
+import http from "http";
+import { errorController } from "./controllers/errorController";
+
+dotenv.config();
+
+const app = express();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(cors({ origin: process.env.FRONTEND_URL }));
+} else {
+  app.use(cors());
+}
+
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(logger("dev"));
+
+// API Routes here
+
+app.use(errorController);
+
+app.use("*", (req: Request, res: Response) => {
+  res.status(404).json({
+    status: "fail",
+    message: "Endpoint not found!",
+  });
+});
+
+const PORT = 8000 || process.env.PORT;
+
+server.listen(PORT, () => {
+  console.log(`SEG server running on port ${PORT}`);
+});
