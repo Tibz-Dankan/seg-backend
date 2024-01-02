437817c8bba1fcad81536c868dbbfe8ebbfc153b
Tibz-Dankan <tibesigwadankan@gmail.com>
2024-01-02 19:22:41 +0300

    test(getTokens): add tests for get token endpoints

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,14 +3,12 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     include: ["src/routes/**/*.test.ts"],
-    // threads: false,
     setupFiles: ["src/test/setup.ts"],
   },
   resolve: {
     alias: {
-      auth: "/src/auth",
-      quotes: "/src/quotes",
-      lib: "/src/lib",
+      auth: "/src/routes/auth",
+      token: "/src/routes/token",
     },
   },
 });
